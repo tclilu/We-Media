8dd526d9cf54cb52d2165b68436b48bcd22fe540
tclilu <984234055@qq.com>
2018-07-11 20:35:50 +0800

    完成分类信息的增删改查

--- admin/categories.php
+++ admin/categories.php
@@ -1,32 +1,84 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/functions.php';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+get_now_user();
+// 接收参数
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+if ("add" === $action && $_SERVER['REQUEST_METHOD'] === 'POST') {
     // 添加分类
-    add_category();
+    edit_category("add");
+} else if ("update" === $action) {
+    // 编辑数据
+    // 接收id
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
+    if (!empty($id)) {
+        // 显示当前id对应分类的信息
+        $category_ById = category_findById($id);
+    } else {
+        exit("参数错误!");
+    }
+    // 表单提交
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // 执行更新操作
+        edit_category("update",$id);
+        // 显示当前id对应分类更新后的信息
+        $category_ById = show_categoryById($id);
+    }
+} else {
+    // 未传入action参数 或者 有get方式传入的参数但键值不为action
+    if (isset($_GET['action']) || (count($_GET) != 0 && key($_GET) !== "action")) {
+        exit("参数错误!");
+    }
 }
+// 操作完成 显示所有分类信息
+$categories = show();
+
 /**
- * 添加分类的方法
+ * 查询所有分类的方法
+ * @return array 返回二维数组
  */
-function add_category()
-{
+function show() {
+    // 查询所有分类
+    return fetch_all("select * from categories;");
+}
+
+/**
+ * @param $id 当前分类对应id
+ * @return array|null 返回当前id对应的分类
+ */
+function show_categoryById($id) {
+    return fetch_one("select * from categories where id={$id};");
+}
+
+/**
+ * 封装添加和编辑分类的方法
+ * @param $method 要进行的操作 添加或编辑
+ * @param string $id 要进行更新操作的分类对应的id
+ */
+function edit_category($method,$id='') {
     // 校验表单数据
-    if (!isset($_POST['name']) || !isset($_POST['slug'])) {
+    if (empty($_POST['name']) || empty($_POST['slug'])) {
         $GLOBALS['msg'] = "请填写表单相关信息";
         return;
     }
     // 接收表单数据
     $name = $_POST['name'];
     $slug = $_POST['slug'];
-    // 执行添加操作
-    $count = execute("insert into categories values(null,'{$slug}','{$name}');");
-    // 根据添加操作返回结果给出不同提示信息
-    // $count>0则为true,<0为false
-    $GLOBALS['success'] = ($count > 0);
-    $GLOBALS['msg'] = $count <= 0 ? "添加失败！" : "添加成功！";
+    // 根据method做出响应
+    if ("add" === $method) {
+        // 执行添加操作
+        $count = execute("insert into categories values(null,'{$slug}','{$name}',1);");
+        // 根据添加操作返回结果给出不同提示信息
+        // $count>0则为true,<0为false
+        $GLOBALS['success'] = ($count > 0);
+        $GLOBALS['msg'] = $count <= 0 ? "添加失败!" : "添加成功!";
+    } else if ("update" === $method && isset($id)) {
+        $count = execute("update categories set category_name='{$name}',slug='{$slug}' where id={$id};");
+        // 根据添加操作返回结果给出不同提示信息
+        // $count>0则为true,<0为false
+        $GLOBALS['success'] = ($count > 0);
+        $GLOBALS['msg'] = $count < 0 ? "保存失败！" : "保存成功！";
+    }
 }
-
-// 查询所有分类
-$categories = fetch_all("select * from categories;");
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -49,40 +101,39 @@ $categories = fetch_all("select * from categories;");
             <h1>分类目录</h1>
         </div>
         <!-- 有错误信息时展示 -->
-        <?php if (isset($msg)){ ?>
-            <?php if ($success){ ?>
+        <?php if (isset($msg)) { ?>
+            <?php if (isset($success)) { ?>
                 <div class="alert alert-success">
-                    <strong>成功！</strong><?php echo $msg;?>
+                    <strong>成功！</strong><?php echo $msg; ?>
                 </div>
-            <?php }else{ ?>
-            <div class="alert alert-danger">
-                <strong>错误！</strong><?php echo $msg;?>
-            </div>
-        <?php }} ?>
+            <?php } else { ?>
+                <div class="alert alert-danger">
+                    <strong>错误！</strong><?php echo $msg; ?>
+                </div>
+            <?php }
+        } ?>
         <div class="row">
             <div class="col-md-4">
-                <form action="" method="post">
-                    <h2>添加新分类目录</h2>
+                <form action="<?php echo isset($category_ById) ? $_SERVER['PHP_SELF'] . '?action=update&id=' . $category_ById['id'] : $_SERVER['PHP_SELF'] . '?action=add' ?>" method="post">
+                    <h2><?php echo isset($category_ById) ? "编辑分类《" . $category_ById['category_name'] . "》" : "添加新分类目录"; ?></h2>
                     <div class="form-group">
                         <label for="name">名称</label>
-                        <input id="name" class="form-control" name="name" type="text" placeholder="分类名称"
-                               autocomplete="off">
+                        <input id="name" class="form-control" name="name" type="text" placeholder="分类名称" autocomplete="off" value="<?php echo isset($category_ById) ? $category_ById['category_name'] : ''; ?>"/>
                     </div>
                     <div class="form-group">
                         <label for="slug">别名</label>
-                        <input id="slug" class="form-control" name="slug" type="text" placeholder="slug"
-                               autocomplete="off">
+                        <input id="slug" class="form-control" name="slug" type="text" placeholder="slug" autocomplete="off" value="<?php echo isset($category_ById) ? $category_ById['slug'] : ''; ?>"/>
                         <p class="help-block">https://zce.me/category/<strong>slug</strong></p>
                     </div>
                     <div class="form-group">
-                        <button class="btn btn-primary" type="submit">添加</button>
+                        <button class="btn btn-primary" type="submit"><?php echo isset($category_ById) ? "保存" : "添加"; ?></button>
                     </div>
                 </form>
             </div>
             <div class="col-md-8">
                 <div class="page-action">
                     <!-- show when multiple checked -->
-                    <a class="btn btn-danger btn-sm" href="javascript:;" style="display: none">批量删除</a>
+                    <a id="del_cate" class="btn btn-danger btn-sm" href="/admin/category_del.php" style="display: none">批量删除</a>
                 </div>
                 <table class="table table-striped table-bordered table-hover">
                     <thead>
@@ -90,22 +141,33 @@ $categories = fetch_all("select * from categories;");
                         <th class="text-center" width="40"><input type="checkbox"></th>
                         <th>名称</th>
                         <th>Slug</th>
+                        <th>状态</th>
                         <th class="text-center" width="100">操作</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php if (empty($categories)) { ?><tr><td colspan="4" style="text-align: center;">~暂无数据~</td></tr><?php } ?>
-                    <?php foreach ($categories as $k => $v){ ?>
+                    <?php if (empty($categories)) { ?>
                         <tr>
-                            <td class="text-center"><input type="checkbox"></td>
+                            <td colspan="5" style="text-align: center;">~暂无数据~</td>
+                        </tr><?php } ?>
+                    <?php foreach ($categories as $k => $v) { ?>
+                        <tr>
+                            <td class="text-center"><input type="checkbox" data-id="<?php echo $v['id']; ?>"></td>
                             <td><?php echo $v['category_name']; ?></td>
                             <td><?php echo $v['slug']; ?></td>
+                            <td><?php if ($v['category_status'] == 1) {
+                                    echo "正常";
+                                } else {
+                                    echo "禁用";
+                                } ?></td>
                             <td class="text-center">
-                                <a href="javascript:;" class="btn btn-info btn-xs">编辑</a>
-                                <a href="javascript:;" class="btn btn-danger btn-xs">删除</a>
+                                <a href="/admin/categories.php?action=update&id=<?php echo $v['id']; ?>"
+                                   class="btn btn-info btn-xs">编辑</a>
+                                <a href="/admin/category_del.php?id=<?php echo $v['id']; ?>"
+                                   class="btn btn-danger btn-xs">删除</a>
                             </td>
                         </tr>
-                    <?php }?>
+                    <?php } ?>
                     </tbody>
                 </table>
             </div>
@@ -115,6 +177,32 @@ $categories = fetch_all("select * from categories;");
 <?php include "./common/aside.php"; ?>
 <script src="/static/assets/vendors/jquery/jquery.js"></script>
 <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
+<script>
+    $(function () {
+        // 提示信息显示5秒自动消失
+        $(".alert").fadeOut(5000);
+        // 初始化复选框数据数组
+        var dataArray = [];
+        // 避免多次重复获取元素
+        var del_btn = $("#del_cate");
+        $("tbody input").on("change", function () {
+            // 根据HTML5元素自定义属性获取对应分类的id
+            var id = $(this).data("id");
+            // 如果选中则将id加入数组，取消选中则从数组中删除
+            if ($(this).prop("checked")) {
+                // 加入数组
+                dataArray.push(id);
+            } else {
+                // 移出id对应下标的数组元素，移除1个元素
+                dataArray.splice(dataArray.indexOf(id), 1);
+            }
+            // 如果数组长度为0，则不显示批量删除按钮，否则显示。
+            (dataArray.length == 0) ? del_btn.fadeOut() : del_btn.fadeIn();
+            // 动态修改按钮链接地址
+            del_btn.prop("search", "?id=" + dataArray);
+        });
+    });
+</script>
 <script>NProgress.done()</script>
 </body>
 </html>
